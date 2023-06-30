e4ab316c37bbb45a462cb519ec1232cde9e42036
mbernal-git <mbernal.sandbox@outlook.com>
2023-06-30 22:50:03 +0800

    added tools and projects section

--- script/script.js
+++ script/script.js
@@ -1,32 +1,65 @@
 var dot1 = document.getElementById("dot1");
 var dot2 = document.getElementById("dot2");
 var dot3 = document.getElementById("dot3");
+var dot4 = document.getElementById("dot4");
+var dot5 = document.getElementById("dot5");
 
 document.addEventListener("scroll", function (e) {
-    if (window.scrollY === 0 || window.scrollY <= (document.body.scrollHeight * 0.333)) {
-        dot1.style.backgroundColor = "#9DD6D6";
+    if (window.scrollY === 0 || window.scrollY < Math.floor(document.body.scrollHeight * 0.2)) {
+        dot1.style.backgroundColor = "#1B2E35";
         dot2.style.backgroundColor = "white";
         dot3.style.backgroundColor = "white";
-    } else if (window.scrollY >= (document.body.scrollHeight * 0.333) && window.scrollY < (document.body.scrollHeight * 0.666)) {
-        dot1.style.backgroundColor = "white";
-        dot2.style.backgroundColor = "#9DD6D6";
-        dot3.style.backgroundColor = "white";
-    } else if (window.scrollY >= (document.body.scrollHeight * 0.666)) {
-        dot1.style.backgroundColor = "white";
-        dot2.style.backgroundColor = "white";
-        dot3.style.backgroundColor = "#9DD6D6";
+        dot4.style.backgroundColor = "white";
+        dot5.style.backgroundColor = "white";
     }
+     else if (window.scrollY >=  Math.floor(document.body.scrollHeight * 0.2) && window.scrollY <  Math.floor(document.body.scrollHeight * 0.4)) {
+        dot1.style.backgroundColor = "white";
+        dot2.style.backgroundColor = "#1B2E35";
+        dot3.style.backgroundColor = "white";
+        dot4.style.backgroundColor = "white";
+        dot5.style.backgroundColor = "white";
+    }
+     else if (window.scrollY >  Math.floor(document.body.scrollHeight * 0.4) && window.scrollY <=  Math.floor(document.body.scrollHeight * 0.6)) {
+        dot1.style.backgroundColor = "white";
+        dot2.style.backgroundColor = "white";
+        dot3.style.backgroundColor = "#1B2E35";
+        dot4.style.backgroundColor = "white";
+        dot5.style.backgroundColor = "white";
+    } 
+    else if (window.scrollY >  Math.floor(document.body.scrollHeight * 0.6) && window.scrollY <=  Math.floor(document.body.scrollHeight * 0.79)) {
+        dot1.style.backgroundColor = "white";
+        dot2.style.backgroundColor = "white";
+        dot3.style.backgroundColor = "white";
+        dot4.style.backgroundColor = "#1B2E35";
+        dot5.style.backgroundColor = "white";
+    } 
+    else if (window.scrollY > Math.floor(document.body.scrollHeight * 0.79)) {
+        dot1.style.backgroundColor = "white";
+        dot2.style.backgroundColor = "white";
+        dot3.style.backgroundColor = "white";
+        dot4.style.backgroundColor = "white";
+        dot5.style.backgroundColor = "#1B2E35";
+    } 
 });
-
 
 dot1.addEventListener("click", function(e) {
     window.scrollTo(0, 0);
 });
 
 dot2.addEventListener("click", function(e) {
-    window.scrollTo(0, (document.body.scrollHeight * 0.3333333));
+    window.scrollTo(0,  (document.body.scrollHeight * 0.2));
 });
 
 dot3.addEventListener("click", function(e) {
-    window.scrollTo(0, (document.body.scrollHeight * 0.66666666666));
+    window.scrollTo(0,  (document.body.scrollHeight * 0.4));
 });
+
+dot4.addEventListener("click", function(e) {
+    window.scrollTo(0,  (document.body.scrollHeight * 0.6));
+});
+
+dot5.addEventListener("click", function(e) {
+    window.scrollTo(0,  (document.body.scrollHeight * 0.8));
+});
+
+
